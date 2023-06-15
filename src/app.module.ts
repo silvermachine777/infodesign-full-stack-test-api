a2aeadb66d7d5478fd8f62cc979186568d6d9456
silvermachine777 <sebastian_gg@outlook.com>
2023-06-15 14:43:46 -0500

    feat(modules): Updating app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+//import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { enviroments } from './environments';
+import config from './config';
+import { HistoricalConsumptionModule } from './historical-consumption/historicalConsumption.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+    }),
+    HistoricalConsumptionModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
