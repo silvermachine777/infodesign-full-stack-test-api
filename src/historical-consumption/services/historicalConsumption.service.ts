10a4893d819c8348a7e15c44540b93f0fb92eed3
silvermachine777 <sebastian_gg@outlook.com>
2023-06-15 20:46:08 -0500

    feat(services): Updating historicalConsumption service

--- src/historical-consumption/services/historicalConsumption.service.ts
+++ src/historical-consumption/services/historicalConsumption.service.ts
@@ -11,54 +11,60 @@ export class HistoricalConsumptionService {
   async byTranches(data: HistorialConsumptionDto) {
     const initialDate = data.initialDate;
     const finalDate = data.finalDate;
+    const historiType = data.historyType;
 
-    try {
-      const request = new sql.Request(this.sqlServerConnection);
+    if (historiType === 'historyByTranches') {
+      try {
+        const request = new sql.Request(this.sqlServerConnection);
 
-      const res = await request.query(`
-        SELECT 
-          l.nombre Linea,
-          hc.fecha Fecha,
-          hc.Consumo,
-          hc.Perdida,
-          hc.Costo [Costo x Consumo]	
-        FROM 
-          HistoricoConsumos hc
-          INNER JOIN Tipo_clientes tc ON tc.id = hc.idTipCliente
-          INNER JOIN Lineas l ON l.id = hc.idLinea
-        WHERE hc.fecha >= '${initialDate}' AND hc.fecha <= '${finalDate}';
-      `);
+        const res = await request.query(`
+          SELECT 
+            l.nombre Linea,
+            hc.fecha Fecha,
+            hc.Consumo,
+            hc.Perdida,
+            hc.Costo [Costo x Consumo]	
+          FROM 
+            HistoricoConsumos hc
+            INNER JOIN Tipo_clientes tc ON tc.id = hc.idTipCliente
+            INNER JOIN Lineas l ON l.id = hc.idLinea
+          WHERE hc.fecha >= '${initialDate}' AND hc.fecha <= '${finalDate}';
+        `);
 
-      return res.recordset.length > 0 ? res.recordset : [];
-    } catch (err) {
-      throw new Error(err);
+        return res.recordset.length > 0 ? res.recordset : [];
+      } catch (err) {
+        throw new Error(err);
+      }
     }
   }
 
   async byClient(data: HistorialConsumptionDto) {
     const initialDate = data.initialDate;
     const finalDate = data.finalDate;
+    const historiType = data.historyType;
 
-    try {
-      const request = new sql.Request(this.sqlServerConnection);
+    if (historiType === 'historyByClient') {
+      try {
+        const request = new sql.Request(this.sqlServerConnection);
 
-      const res = await request.query(`
-        SELECT 
-            tc.nombre Cliente,
-            l.nombre Linea,
-            hc.Consumo,
-            hc.Perdida,
-            hc.Costo [Costo x Consumo]	
-        FROM 
-          HistoricoConsumos hc
-          INNER JOIN Tipo_clientes tc ON tc.id = hc.idTipCliente
-          INNER JOIN Lineas l ON l.id = hc.idLinea
-        WHERE hc.fecha >= '${initialDate}' AND hc.fecha <= '${finalDate}'
-      `);
+        const res = await request.query(`
+          SELECT 
+              tc.nombre Cliente,
+              l.nombre Linea,
+              hc.Consumo,
+              hc.Perdida,
+              hc.Costo [Costo x Consumo]	
+          FROM 
+            HistoricoConsumos hc
+            INNER JOIN Tipo_clientes tc ON tc.id = hc.idTipCliente
+            INNER JOIN Lineas l ON l.id = hc.idLinea
+          WHERE hc.fecha >= '${initialDate}' AND hc.fecha <= '${finalDate}'
+        `);
 
-      return res.recordset.length > 0 ? res.recordset : [];
-    } catch (err) {
-      throw new Error(err);
+        return res.recordset.length > 0 ? res.recordset : [];
+      } catch (err) {
+        throw new Error(err);
+      }
     }
   }
 }
